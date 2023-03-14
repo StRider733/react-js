381555a839ab9a243262eebb0e1924bb790afda7
StRider733 <118477683+StRider733@users.noreply.github.com>
2023-03-14 23:40:27 +0300

    h

--- lesson6/meme-generator/src/App.js
+++ lesson6/meme-generator/src/App.js
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar';
 import Meme from './components/Meme';
 import Counter from './components/Counter';
 import Form from './components/Form';
-import Signup from './components/Signup';
+// import Signup from './components/Signup';
 import Ternary from './components/Ternary';
 import UseEffect from './components/UseEffect';
 
